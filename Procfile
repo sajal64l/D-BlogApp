f0d786871d729ea43a9bf5c86aaf68f758a62f9f
SAJAL PANDEY <sajalpandey61@gmail.com>
2021-06-11 00:43:21 +0545

    ch(deploy): Setup deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,3 @@
+release: python manage.py makemigrations --no-input
+release: python manage.py migrate --no-input
+web: gunicorn djangoapi.wsgi
--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR_OLD, 'media')
 MEDIA_URL ='/media/'
+
+django_heroku.settings(locals())
